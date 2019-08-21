1f00865f44773cf632df02f57986152619e3251f
KSM13 <KSM13@DESKTOP-FMDGTE7>
2019-08-21 17:47:11 +0900

    HR VO 추가

---
+++ ERPPJ_PJ/src/main/java/com/pj/erp/vo/HR_VO.java
@@ -0,0 +1,163 @@
+package com.pj.erp.vo;
+
+public class HR_VO {
+
+	private String username;
+	private String password;
+	private String e_picture;
+	private String e_engName;
+	private String e_name;
+	private String e_code;
+	private int e_gender;
+	private String e_born;
+	private String e_hp;
+	private String e_phone;
+	private String e_address;
+	private String e_address2;
+	private String e_endAddress;
+	private String e_email;
+	private String e_nfcCodeNFC;
+	private int household;
+	private int disClass;
+	
+	public HR_VO() {}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getE_picture() {
+		return e_picture;
+	}
+
+	public void setE_picture(String e_picture) {
+		this.e_picture = e_picture;
+	}
+
+	public String getE_engName() {
+		return e_engName;
+	}
+
+	public void setE_engName(String e_engName) {
+		this.e_engName = e_engName;
+	}
+
+	public String getE_name() {
+		return e_name;
+	}
+
+	public void setE_name(String e_name) {
+		this.e_name = e_name;
+	}
+
+	public String getE_code() {
+		return e_code;
+	}
+
+	public void setE_code(String e_code) {
+		this.e_code = e_code;
+	}
+
+	public int getE_gender() {
+		return e_gender;
+	}
+
+	public void setE_gender(int e_gender) {
+		this.e_gender = e_gender;
+	}
+
+	public String getE_born() {
+		return e_born;
+	}
+
+	public void setE_born(String e_born) {
+		this.e_born = e_born;
+	}
+
+	public String getE_hp() {
+		return e_hp;
+	}
+
+	public void setE_hp(String e_hp) {
+		this.e_hp = e_hp;
+	}
+
+	public String getE_phone() {
+		return e_phone;
+	}
+
+	public void setE_phone(String e_phone) {
+		this.e_phone = e_phone;
+	}
+
+	public String getE_address() {
+		return e_address;
+	}
+
+	public void setE_address(String e_address) {
+		this.e_address = e_address;
+	}
+
+	public String getE_address2() {
+		return e_address2;
+	}
+
+	public void setE_address2(String e_address2) {
+		this.e_address2 = e_address2;
+	}
+
+	public String getE_endAddress() {
+		return e_endAddress;
+	}
+
+	public void setE_endAddress(String e_endAddress) {
+		this.e_endAddress = e_endAddress;
+	}
+
+	public String getE_email() {
+		return e_email;
+	}
+
+	public void setE_email(String e_email) {
+		this.e_email = e_email;
+	}
+
+	public String getE_nfcCodeNFC() {
+		return e_nfcCodeNFC;
+	}
+
+	public void setE_nfcCodeNFC(String e_nfcCodeNFC) {
+		this.e_nfcCodeNFC = e_nfcCodeNFC;
+	}
+
+	public int getHousehold() {
+		return household;
+	}
+
+	public void setHousehold(int household) {
+		this.household = household;
+	}
+
+	public int getDisClass() {
+		return disClass;
+	}
+
+	public void setDisClass(int disClass) {
+		this.disClass = disClass;
+	}
+	
+	
+	
+}
