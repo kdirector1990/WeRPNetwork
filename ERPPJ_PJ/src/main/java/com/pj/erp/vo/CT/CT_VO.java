cb17f7f955ba7ca25761683f8de217dc61ff2c96
김지원 <skawk0264@naver.com>
2019-09-04 20:17:53 +0900

    전산 DB 고정자산 입력 (상세계정 테이블 임시 값들 들어오면, mapper 수정해야함.)

--- ERPPJ_PJ/src/main/java/com/pj/erp/vo/CT/CT_VO.java
+++ ERPPJ_PJ/src/main/java/com/pj/erp/vo/CT/CT_VO.java
@@ -1,4 +1,4 @@
-package com.pj.erp.vo;
+package com.pj.erp.vo.CT;
 
 import java.sql.Date;
 
@@ -6,7 +6,6 @@ public class CT_VO {
 
 	//멤버변수
 	private String ceq_code;			//전산설비코드
-	private String account_code; 		//계정코드
 	private String ceq_name;			//설비명
 	private String ceq_type;			//보유구분(1: 보유 2: 대여 3: 폐기))
 	private Date ceq_acquire_date; 		//구입일
@@ -14,10 +13,8 @@ public class CT_VO {
 	private String ceq_location;		//위치
 	private int ceq_prime_cost;			//매입가
 	private int ceq_durable;			//예상연수내용
-	private Date ceq_evaluation_date; 	//최근평가일
-	private int ceq_evaluation_price; 	//최근평가액
 	private int ceq_depreciation; 		//감가상각여부
-	private String ceq_deprecition_type;//감가상각법
+	private String ceq_depreciation_type;//감가상각법
 	
 	//생성자
 	public CT_VO() {}
@@ -30,14 +27,6 @@ public class CT_VO {
 
 	public void setCeq_code(String ceq_code) {
 		this.ceq_code = ceq_code;
-	}
-
-	public String getAccount_code() {
-		return account_code;
-	}
-
-	public void setAccount_code(String account_code) {
-		this.account_code = account_code;
 	}
 
 	public String getCeq_name() {
@@ -96,22 +85,6 @@ public class CT_VO {
 		this.ceq_durable = ceq_durable;
 	}
 
-	public Date getCeq_evaluation_date() {
-		return ceq_evaluation_date;
-	}
-
-	public void setCeq_evaluation_date(Date ceq_evaluation_date) {
-		this.ceq_evaluation_date = ceq_evaluation_date;
-	}
-
-	public int getCeq_evaluation_price() {
-		return ceq_evaluation_price;
-	}
-
-	public void setCeq_evaluation_price(int ceq_evaluation_price) {
-		this.ceq_evaluation_price = ceq_evaluation_price;
-	}
-
 	public int getCeq_depreciation() {
 		return ceq_depreciation;
 	}
@@ -120,12 +93,12 @@ public class CT_VO {
 		this.ceq_depreciation = ceq_depreciation;
 	}
 
-	public String getCeq_deprecition_type() {
-		return ceq_deprecition_type;
+	public String getCeq_depreciation_type() {
+		return ceq_depreciation_type;
 	}
 
-	public void setCeq_deprecition_type(String ceq_deprecition_type) {
-		this.ceq_deprecition_type = ceq_deprecition_type;
+	public void setCeq_depreciation_type(String ceq_depreciation_type) {
+		this.ceq_depreciation_type = ceq_depreciation_type;
 	}
 	
 	
