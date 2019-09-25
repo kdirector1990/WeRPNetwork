f4fad2ca245a10325abed00b200973f92eafd403
spoat <spoat@172.30.91.77>
2019-09-25 14:44:04 +0900

    블록체인 기초설정 지갑 설정, 이더금액 설정해놨음. 구매할 페이지, 예산할당 짜봐야함.

--- ERPPJ_PJ/src/main/java/com/pj/erp/service/MateralServiceImpl.java
+++ ERPPJ_PJ/src/main/java/com/pj/erp/service/MateralServiceImpl.java
@@ -45,22 +45,65 @@ public class MateralServiceImpl {
     
     // 이더를 전송하는 메소드
     @SuppressWarnings("deprecation")
-	public void payCreateClub(HttpServletRequest req) throws Exception {
+	public void payMaterial(HttpServletRequest req) throws Exception {
     	// 구매하는 부서의 코드로 구매하게 만든다. 
     	String deptcode = (String) req.getSession().getAttribute("department_code");
-    	// 2번째 계정의 primary key를 접속한 동호회 개설자에게 부여한다.
-    	Credentials new_credentials = Credentials.create("e1bc6e0fc06696e454b1c42286b0b5b0ddcf4eba190976a24e9645924f70485a");
+    	
+    	// 계정의 primary key를 접속한 부서별로 할당한다.
+    	Credentials dept_AccountNumber = null; 
+    	
+    	if(deptcode == "MS_dept") {
+    		dept_AccountNumber = Credentials.create("3f0b5c58378de554534a5a8c630aac075886e74a6b3229000ae78f4500e153e3"); //경영지원
+    	}
+    	else if(deptcode == "CT_dept") {
+    		dept_AccountNumber = Credentials.create("3b69cc479dbac9b02d2b7c39f7829a2e5df850203a1766355d854dd89fcc6848"); //전산
+    	}
+    	else if(deptcode == "FT_dept") {
+    		dept_AccountNumber = Credentials.create("3f1e2bd4ef8941731d244359f0cdf1ef079e5eaffd57ea6d31addeb55e20d426"); //재무
+    	}
+    	else if(deptcode == "HR_dept") {
+    		dept_AccountNumber = Credentials.create("83e094366642f531189d56df33ac870db53af8c7f7f60a7a8b20cb85bc43a59f"); //인사
+    	}
+    	else if(deptcode == "MF_dept") {
+    		dept_AccountNumber = Credentials.create("c6fd20908cdc2326a8a5e366228c149fa7632e9c4ef035f5b7ebee1a04158b7e"); //제조
+    	}
+    	else if(deptcode == "ST_dept") {
+    		dept_AccountNumber = Credentials.create("bfde674ae42a9fa179e3e965e1ddb9ea479e63e62cb61f16c2adc4b365314bc5"); //영업
+    	}
+    	
+    	//구매하는 가격을 입력받아서 조건에 해당하는 이더를 거래하도록 설정한다.
+    	int price= Integer.parseInt(req.getParameter("price"));
+    	BigInteger ethers = null;
+    	
+    	//가격에 해당하는 이더 설정.
+    	if(price < 100000) {
+    		ethers = etherToWei(new BigDecimal(5));
+    	}
+    	else if((100000 < price) && (price < 300000)) {
+    		ethers = etherToWei(new BigDecimal(7));
+    	}
+    	else if ((300000 < price) && (price < 500000)) {
+    		ethers = etherToWei(new BigDecimal(15));
+    	}
+    	else if ((500000 < price) && (price < 800000)) {
+    		ethers = etherToWei(new BigDecimal(25));
+    	}
+    	else if ((800000 < price) && (price < 1000000)) {
+    		ethers = etherToWei(new BigDecimal(40));
+    	}
+    	else {
+    		ethers = etherToWei(new BigDecimal(50));
+    	}
 
     	byte[] name = stringToBytes32(deptcode); 
-    	BigInteger price = etherToWei(new BigDecimal(1)); // 개설비는 1이더로 고정하기위함
     	
     	// 자바로 변환된 CreateClub의 메소드(load)를 호출하여 사용 : 이더 전송
     	// 첫번째 매개변수인 contractAddress는 deploy메소드에서얻은 계약주소 
-    	Materal club = Materal.load(contractAddress, web3j, new_credentials, gasPrice, gasLimit);
-    	// 솔리디티의 newCreateClub을 호출 : 두번째 계정인 new_credentials이 host에게 1이더를 전송하기위함
+    	Materal dept = Materal.load(contractAddress, web3j, dept_AccountNumber, gasPrice, gasLimit);
+    	// 솔리디티의 buyMaterial을 호출 : 부서에 해당하는 계정에서 금액에 맞추어서 호스트에 (임시적)으로 해당 이더를 전송하게 만들어둠. 
     	// 첫번재 매개변수는 매물id인데 사용하지않아 상관없으므로 0으로 초기화
-    	// 두번째 매개변수는 현재 접속한 세션의 아이디    	
-		club.buyMaterial(new BigInteger("0"), name, price).send();
+    	// 두번째 매개변수는 현재 접속한 부서코드
+		dept.buyMaterial(new BigInteger("0"), name, ethers).send();
 		
     }
 	
