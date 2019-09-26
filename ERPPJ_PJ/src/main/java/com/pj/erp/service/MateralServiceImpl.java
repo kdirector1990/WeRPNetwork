c5fb03c03386b8c45ab910d18df8d0a9b6ba6383
김지원 <skawk0264@naver.com>
2019-09-26 11:13:10 +0900

    가나슈에서 Infura로 서버 바꿈, VO도 이더리움 테이블로 변환했음.

--- ERPPJ_PJ/src/main/java/com/pj/erp/service/MateralServiceImpl.java
+++ ERPPJ_PJ/src/main/java/com/pj/erp/service/MateralServiceImpl.java
@@ -22,13 +22,21 @@ public class MateralServiceImpl {
 	ERPDAO dao;
 	
 	// 가나슈와 연결 한다 : localhost:8545
-	private static final Web3j web3j = Web3j.build(new HttpService("http://localhost:8545"));
-	// 호스트에 가나슈 첫번째 계정의  PRIVATE KEY 복사하여 연결
-	private static final Credentials hostCredentials = Credentials.create("666a82fc33f8134577a7beb1bdeaa689bb72740178727691d63032432b83e0fb");
+	private static final Web3j web3j = Web3j.build(new HttpService("https://mainnet.infura.io/v3/d11459c1c17049628f462a1492c7df36"));
+	// 호스트에 가나슈 첫번째 계정의  PRIVATE KEY 복사하여 연결(재무팀)
+	private static final Credentials hostCredentials = Credentials.create("666A82FC33F8134577A7BEB1BDEAA689BB72740178727691D63032432B83E0FB");
+	
+	//거래처 지갑을 변수로서 생성한다.
+	private static final Credentials account = Credentials.create("BFDE674AE42A9FA179E3E965E1DDB9EA479E63E62CB61F16C2ADC4B365314BC5");
+	private static final Credentials account2 = Credentials.create("53458B07979175D41C83DF78CA6A4B052C178E2D3EDA432C5A7D130E5716DE9F");
+	
 	// 가나슈의 gasLimit과 gasPrice를 적어준다.
 	private static final BigInteger gasLimit = new BigInteger("6721975");
 	private static final BigInteger gasPrice = new BigInteger("20000000000");
+	
+	//계약자의 이름
 	String contractAddress = "";
+	String contractAddress2 = "";
     
 	// etherToWei
 	public static BigInteger etherToWei(BigDecimal ether) {
@@ -43,46 +51,42 @@ public class MateralServiceImpl {
         System.arraycopy(byteValue, 0, byteValueLen32, 0, byteValue.length);
         return byteValueLen32;
     }
-		
-    // 배포함으로서 가나슈에서 Transaction -> CONTRACT CREATION ->  CONTRACT ADDRESS 가 발생하고 정보를 와서 변수에 담고 DB저장하여 사용할수 있다.(사용안함)
+    
+    //예산편성 시 작성할 계약서
+    //배포함으로서 가나슈에서 Transaction -> CONTRACT CREATION ->  CONTRACT ADDRESS 가 발생하고 정보를 와서 변수에 담고 DB저장하여 사용할수 있다.(사용안함)
     @SuppressWarnings("deprecation")
-	public void deploy() throws Exception {    	
+    public void deploy2() throws Exception {    	
     	contractAddress = Materal.deploy(web3j, hostCredentials, gasPrice, gasLimit).send().getContractAddress();
     	//  System.out.println(contractAddress);    	    	
+    }
+		
+    @SuppressWarnings("deprecation")
+	public void budgetAdd(HttpServletRequest req) throws Exception {
+    	String department_code = req.getParameter("department_code");
+    	
+    	BlockChainVO vo = dao.getGroupCode(department_code);
+    		
     }
     
     // 이더를 전송하는 메소드
     @SuppressWarnings("deprecation")
 	public void payMaterial(HttpServletRequest req) throws Exception {
+    	
+    	//계약서 작성.
+    	contractAddress2 = Materal.deploy(web3j, hostCredentials, gasPrice, gasLimit).send().getContractAddress();
+    	
+    	
     	// 구매하는 부서의 코드로 구매하게 만든다. 
     	String department_code = (String) req.getSession().getAttribute("dCode");
     	
     	// department_code를 통해 department_group_code를 가져온다.
     	// 팀 코드를 통해 부서코드를 가져온다.
     	BlockChainVO bc = dao.getGroupCode(department_code);
-    	String deptcode = bc.getDepartment_group_code();
+    	String deptWallet = bc.getWallet_code();
+    	String deptname = bc.getDepartment_name();
     	
     	// 계정의 primary key를 접속한 부서별로 할당한다.
-    	Credentials dept_AccountNumber = null; 
-    	
-    	if(deptcode == "MS_dept") {
-    		dept_AccountNumber = Credentials.create("3f0b5c58378de554534a5a8c630aac075886e74a6b3229000ae78f4500e153e3"); //경영지원
-    	}
-    	else if(deptcode == "CT_dept") {
-    		dept_AccountNumber = Credentials.create("3b69cc479dbac9b02d2b7c39f7829a2e5df850203a1766355d854dd89fcc6848"); //전산
-    	}
-    	else if(deptcode == "FT_dept") {
-    		dept_AccountNumber = Credentials.create("3f1e2bd4ef8941731d244359f0cdf1ef079e5eaffd57ea6d31addeb55e20d426"); //재무
-    	}
-    	else if(deptcode == "HR_dept") {
-    		dept_AccountNumber = Credentials.create("83e094366642f531189d56df33ac870db53af8c7f7f60a7a8b20cb85bc43a59f"); //인사
-    	}
-    	else if(deptcode == "MF_dept") {
-    		dept_AccountNumber = Credentials.create("c6fd20908cdc2326a8a5e366228c149fa7632e9c4ef035f5b7ebee1a04158b7e"); //제조
-    	}
-    	else if(deptcode == "ST_dept") {
-    		dept_AccountNumber = Credentials.create("bfde674ae42a9fa179e3e965e1ddb9ea479e63e62cb61f16c2adc4b365314bc5"); //영업
-    	}
+    	Credentials dept_AccountNumber = Credentials.create(deptWallet); 
     	
     	//구매하는 가격을 입력받아서 조건에 해당하는 이더를 거래하도록 설정한다.
     	int price= Integer.parseInt(req.getParameter("price"));
@@ -90,32 +94,33 @@ public class MateralServiceImpl {
     	
     	//가격에 해당하는 이더 설정.
     	if(price < 100000) {
-    		ethers = etherToWei(new BigDecimal(5));
+    		ethers = etherToWei(new BigDecimal(1));
     	}
     	else if((100000 < price) && (price < 300000)) {
-    		ethers = etherToWei(new BigDecimal(7));
+    		ethers = etherToWei(new BigDecimal(2));
     	}
     	else if ((300000 < price) && (price < 500000)) {
-    		ethers = etherToWei(new BigDecimal(15));
+    		ethers = etherToWei(new BigDecimal(3));
     	}
     	else if ((500000 < price) && (price < 800000)) {
-    		ethers = etherToWei(new BigDecimal(25));
+    		ethers = etherToWei(new BigDecimal(4));
     	}
     	else if ((800000 < price) && (price < 1000000)) {
-    		ethers = etherToWei(new BigDecimal(40));
+    		ethers = etherToWei(new BigDecimal(5));
     	}
     	else {
-    		ethers = etherToWei(new BigDecimal(50));
+    		ethers = etherToWei(new BigDecimal(6));
     	}
 
-    	byte[] name = stringToBytes32(deptcode); 
+    	byte[] name = stringToBytes32(deptname); 
     	
     	// 자바로 변환된 CreateClub의 메소드(load)를 호출하여 사용 : 이더 전송
     	// 첫번째 매개변수인 contractAddress는 deploy메소드에서얻은 계약주소 
-    	Materal dept = Materal.load(contractAddress, web3j, dept_AccountNumber, gasPrice, gasLimit);
+    	Materal dept = Materal.load(contractAddress2, web3j, dept_AccountNumber, gasPrice, gasLimit);
+
     	// 솔리디티의 buyMaterial을 호출 : 부서에 해당하는 계정에서 금액에 맞추어서 호스트에 (임시적)으로 해당 이더를 전송하게 만들어둠. 
     	// 첫번재 매개변수는 매물id인데 사용하지않아 상관없으므로 0으로 초기화
-    	// 두번째 매개변수는 현재 접속한 부서코드
+    	// 두번째 매개변수는 현재 접속한 부서코드 이름.
 		dept.buyMaterial(new BigInteger("0"), name, ethers).send();
 		
     }
--- ERPPJ_PJ/src/main/java/com/pj/erp/vo/BlockChainVO.java
+++ ERPPJ_PJ/src/main/java/com/pj/erp/vo/BlockChainVO.java
@@ -3,10 +3,10 @@ package com.pj.erp.vo;
 public class BlockChainVO {
 	
 	//변수
+	String ether_code;
 	String department_code;
-	String department_name;
-	String department_group_code;
 	String Wallet_code;
+	String department_name;
 	
 	//생성자
 	public void BlockChainVO() {}
@@ -20,22 +20,6 @@ public class BlockChainVO {
 		this.department_code = department_code;
 	}
 
-	public String getDepartment_name() {
-		return department_name;
-	}
-
-	public void setDepartment_name(String department_name) {
-		this.department_name = department_name;
-	}
-
-	public String getDepartment_group_code() {
-		return department_group_code;
-	}
-
-	public void setDepartment_group_code(String department_group_code) {
-		this.department_group_code = department_group_code;
-	}
-
 	public String getWallet_code() {
 		return Wallet_code;
 	}
@@ -43,7 +27,22 @@ public class BlockChainVO {
 	public void setWallet_code(String wallet_code) {
 		Wallet_code = wallet_code;
 	}
-	
+
+	public String getEther_code() {
+		return ether_code;
+	}
+
+	public void setEther_code(String ether_code) {
+		this.ether_code = ether_code;
+	}
+
+	public String getDepartment_name() {
+		return department_name;
+	}
+
+	public void setDepartment_name(String department_name) {
+		this.department_name = department_name;
+	}
 	
 
 }
