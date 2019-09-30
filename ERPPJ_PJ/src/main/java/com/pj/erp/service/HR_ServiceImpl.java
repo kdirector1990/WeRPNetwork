a523b9f9e27543a7214d18c8964cb094438b01c1
Kwon_SY <kwonsoonyong90@gmail.com>
2019-09-30 15:11:01 +0900

    파일 경로 수정

--- ERPPJ_PJ/src/main/java/com/pj/erp/service/HR_ServiceImpl.java
+++ ERPPJ_PJ/src/main/java/com/pj/erp/service/HR_ServiceImpl.java
@@ -53,9 +53,9 @@ public class HR_ServiceImpl implements HR_Service {
 		MultipartFile file = req.getFile("e_picture");
         
         String saveDir = req.getRealPath("/resources/hr_img/"); 
-		/* F:\\dev50\\git\\WeRPNetwork\\ERPPJ_PJ\\src\\main\\webapp\\resources\\hr_img\\   
+		/* F:\dev50\git\WeRPNetwork\ERPPJ_PJ\src\main\webapp\resources\hr_img\ 
 		 * 서버용 저장 경로*/
-        String realDir="C:\\Users\\KSM13\\git\\WeRPNetwork\\ERPPJ_PJ\\src\\main\\webapp\\resources\\hr_img\\"; // 저장 경로
+        String realDir="F:\\dev50\\git\\WeRPNetwork\\ERPPJ_PJ\\src\\main\\webapp\\resources\\hr_img\\"; // 저장 경로
  
         try {
             file.transferTo(new File(saveDir+file.getOriginalFilename()));            
