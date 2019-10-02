cd7cc4935e0e0ef20da0a953c321450654c3b353
Kwon_SY <kwonsoonyong90@gmail.com>
2019-10-02 12:44:56 +0900

    파일 저장 경로 변경

--- ERPPJ_PJ/src/main/java/com/pj/erp/service/HR_ServiceImpl.java
+++ ERPPJ_PJ/src/main/java/com/pj/erp/service/HR_ServiceImpl.java
@@ -55,7 +55,7 @@ public class HR_ServiceImpl implements HR_Service {
         String saveDir = req.getRealPath("/resources/hr_img/"); 
 		/* F:\dev50\git\WeRPNetwork\ERPPJ_PJ\src\main\webapp\resources\hr_img\ 
 		 * 서버용 저장 경로*/
-        String realDir="C:\\Users\\KSM13\\git\\WeRPNetwork\\ERPPJ_PJ\\src\\main\\webapp\\resources\\hr_img\\"; // 저장 경로
+        String realDir="F:\\dev50\\git\\WeRPNetwork\\ERPPJ_PJ\\src\\main\\webapp\\resources\\hr_img\\ "; // 저장 경로
         String times = String.valueOf(System.currentTimeMillis());
         try {
             file.transferTo(new File(saveDir+times+uuid+"_"+file.getOriginalFilename()));            
@@ -341,7 +341,7 @@ public class HR_ServiceImpl implements HR_Service {
 	    
 	    String saveDir = req.getRealPath("/resources/hr_img/");
 
-	    String realDir = "C:\\Users\\KSM13\\git\\WeRPNetwork\\ERPPJ_PJ\\src\\main\\webapp\\resources\\hr_img\\"; 
+	    String realDir = "F:\\dev50\\git\\WeRPNetwork\\ERPPJ_PJ\\src\\main\\webapp\\resources\\hr_img\\ "; 
 	    /* "F:\\dev50\\git\\WeRPNetwork\\ERPPJ_PJ\\src\\main\\webapp\\resources\\hr_img"; */	  
 	    String times = String.valueOf(System.currentTimeMillis());
 	    try {	    	
