d339a86d6c88566202255005dc8e6f33eeb56425
YooJoonSeon <YooJoonSeon@JOONSEON>
2019-10-02 16:55:57 +0900

    풀받을라고 커밋

--- ERPPJ_PJ/src/main/java/com/pj/erp/controller/ST_Controller.java
+++ ERPPJ_PJ/src/main/java/com/pj/erp/controller/ST_Controller.java
@@ -576,23 +576,6 @@ public class ST_Controller {
 	}
 	
 	//부서 조회 팝업창
-	@RequestMapping("FT_searchDepartmentname")
-	public String FT_searchDepartmentname(HttpServletRequest req, Model model) {
-		logger.info("log => ST_searchDepartmentname");
-		
-		return "ST/ST_searchDepartmentname4";
-	}
-	
-	//부서 조회 팝업창 결과
-	@RequestMapping("FT_searchDepartmentname_result")
-	public String FT_searchDepartmentname_result(HttpServletRequest req, Model model) {
-		logger.info("log => ST_searchDepartmentname_result");
-		service.searchDepCode(req, model);
-		
-		return "ST/ST_searchDepartmentname_result";
-	}
-
-	//부서 조회 팝업창
 	@RequestMapping("FT_searchDepartmentname2")
 	public String FT_searchDepartmentname2(HttpServletRequest req, Model model) {
 		logger.info("log => FT_searchDepartmentname2");
